31b48365cc4c6936237a057e7f43a08064c79d9e
cjp <cjp_ca@163.com>
2020-12-26 16:39:46 +0800

    offer10-ii

--- offer10/main.go
+++ offer10/main.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func fib(n int) int {
-	var f0,f1 = 0,1
+	var f0,f1 = 1,1
 	for i:=0;i<n;i++{
 		f0,f1 = f1,(f0 + f1)%1000000007
 	}
 	return f0
+}
+
+func numWays(n int) int {
+
 }
 
 func main(){
