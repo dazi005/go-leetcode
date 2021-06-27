b3dbe36d77ee9bce8033804e3a9dd11de8a4350b
cjp <cjp_ca@163.com>
2021-06-27 14:24:54 +0800

    剑指offer38%57

--- offer57和为s的两个数字/main.go
+++ offer57和为s的两个数字/main.go
@@ -4,7 +4,7 @@ func twoSum(nums []int, target int) []int {
 	i, j := 0, len(nums) - 1
 	for i < j {
 		s := nums[i] + nums[j]
-		if s < target {
+		if s < target0 {
 			i ++
 		} else if s > target{
 			j --
